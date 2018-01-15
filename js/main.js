d5a7d7604f5da9bb1420c5ad4c4a1c259c90f4d1
Timmy <timmy@s-Macintosh.fios-router.home>
2018-01-15 07:57:31 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("Up and running!");
+var cardOne = "Queen of Hearts";
+var cardTwo = "Queen of Spades";
+var cardThree = "King of Hearts";
+var cardFour = "King of Spades";
+console.log("User flipped " + cardOne);
