2a82dc516f56d69a64f764923faf4074cccfa561
Timmy <timmy@s-Macintosh.fios-router.home>
2018-01-20 21:06:57 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
 console.log("Up and running!");
+// Create an array to store all cards
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = []
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log('User flipped queen');
-var cardTwo = cards[3];
-cardsInPlay.push('cardTwo');
-console.log('User flipped king');
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
-	}
-}
+
+// Create an array to store the cards in play
+var cardsInPlay = [];
+
+// Create a function to check for a match
+var checkForMatch = function () {
+  // Check to see if two cards match and provide feedback to user
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+
+  }
+};
+
+// Create a function to represent a user flipping a card
+var flipCard = function (cardId) {
+
+  // Display the card the user just flipped in the console.
+  console.log("User flipped " + cards[cardId]);
+
+  // Add card to array of cards that are in play
+  cardsInPlay.push(cards[cardId]);
+
+  // Check to see if two cards have been played
+  if (cardsInPlay.length === 2) {
+    // If so, call the checkForMatch function
+    checkForMatch();
+  }
+};
+
+
+// For now, we are manually calling the flipCard function
+// to represent a user's play
+// Call the flipCard function, passing in an index as the argument
+flipCard(0);
+// Call the flipCard function, passing in an index as the argument
+flipCard(2);
