0dfc6f3f13e79f76b770b45e9ccb493bbcf03128
Timmy <timmy@s-Macintosh.fios-router.home>
2018-01-21 09:26:48 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,43 +1,51 @@
-console.log("Up and running!");
+
 var cards = [
-{
-  rank: "queen",
-  suit: "hearts",
-  cardImage: "images/queen-of-hearts.png"
-},
-{
-  rank: "queen",
-  suit: "diamonds",
-  cardImage: "images/queen-of-diamonds.png"
-},
-{
-  rank: "king",
-  suit: "hearts",
-  cardImage: "images/king-of-hearts.png"
-},
-{
-  rank: "king",
-  suit: "diamonds",
-  cardImage: "images/king-of-diamonds.png"
-}
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
 ];
 var cardsInPlay = [];
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 };
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
+    cardsInPlay = [];
   }
 };
-
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+createBoard();
